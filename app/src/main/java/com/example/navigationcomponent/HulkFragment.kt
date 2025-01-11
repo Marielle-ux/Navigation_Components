5859216504159be5a1f67ced4f093d71a1f61e19
Marielle-ux <vajnbergtomiris@gmail.com>
2025-01-11 20:07:19 +0500

    endless commit

--- app/src/main/java/com/example/navigationcomponent/HulkFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/HulkFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.navigationcomponent.databinding.FragmentCaptainAmericaBinding
 import com.example.navigationcomponent.databinding.FragmentHulkBinding
 
+@Suppress("UNREACHABLE_CODE")
 class HulkFragment : Fragment() {
     private lateinit var binding: FragmentHulkBinding
 
@@ -15,8 +17,13 @@ class HulkFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       binding = FragmentHulkBinding.inflate(layoutInflater, container, false)
-return binding.root
+        binding = FragmentHulkBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_hulkFragment_to_thorFragment)
+        }
     }
 }
 
--- app/src/main/java/com/example/navigationcomponent/NewButton.kt
+++ app/src/main/java/com/example/navigationcomponent/NewButton.kt
@@ -7,26 +7,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.navigationcomponent.databinding.FragmentCaptainAmericaBinding
+import com.example.navigationcomponent.databinding.FragmentNewButtonBinding
 import data.RetrofitService
 import kotlinx.coroutines.launch
+import android.util.Log
 
-class CaptainAmericaFragment : Fragment() {
+class NewButton : Fragment() {
+    private lateinit var binding: FragmentNewButtonBinding
 
-    private lateinit var binding: FragmentCaptainAmericaBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCaptainAmericaBinding.inflate(layoutInflater, container, false)
+        binding = FragmentNewButtonBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button4.setOnClickListener {
-            findNavController().navigate(R.id.action_captainAmericaFragment_to_hulkFragment)
+
+        binding.newButton.setOnClickListener {
+            lifecycleScope.launch {
+                try {
+                    val data = RetrofitService.apiService.getFact()
+                    binding.textViewFact.text = data.text
+                } catch (e: Exception) {
+                    Log.d("Error", e.message.toString())
+                }
+            }
         }
+
     }
 }
